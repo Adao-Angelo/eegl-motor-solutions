9766729c61cc463789eebfa1e400df6a027066c8
Adao-Angelo <adaobegginer@gmail.com>
2024-09-06 11:27:44 +0100

    alert page

--- app/(eegl)/sobrenos/page.tsx
+++ app/(eegl)/sobrenos/page.tsx
@@ -1,3 +1,12 @@
-export default async function Home() {
-  return <main>about</main>;
+export default async function About() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-[200px] p-[200px]">
+      <div className="bg-c-stand text-white p-[50px] rounded-lg shadow-lg">
+        <h1 className="text-[30px] font-black mb-4">ALERTA</h1>
+        <p className="text-lg">
+          Esta página está em desenvolvimento. Por favor, volte mais tarde.
+        </p>
+      </div>
+    </main>
+  );
 }
